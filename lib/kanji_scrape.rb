b2cb67755af3a8fbde0d981e9ede89191dfcef42
David Mah <mahhaha@gmail.com>
2011-08-26 22:30:53 -0700

    Starting Json based data storing
    - Moved html tables to a new directory. They'll be gone soon.
    
    - Added index.html to the root so that users don't have to crawl around
        the directory so much.
    
    - Created data/kanji_json which contains a json string of all the
      definitions for kanji by index
    
    - Reformatted some files without testing

---
+++ lib/kanji_scrape.rb
@@ -0,0 +1,51 @@
+ROOT = "../"
+require 'mechanize'
+WEBSITE = 'http://japanese.about.com/library/'
+def system_call(input)
+  while not s = system(input)
+    sleep(0.5)
+  end
+  s
+end
+
+def process_kanji(k, image_dir, data_dir, compound_dir)
+  definition_agent = Mechanize.new
+  def_url = WEBSITE + k.attr('href')
+
+  children = k.children.to_a
+  #img node is at varying indexes do to crap like <br />
+  while child = children.pop
+    if child.node_name == 'img'
+      image_url = child.attr('src')
+      break
+    end
+  end
+  image_url = WEBSITE + image_url if image_url.partition(/http/)[2] == ""
+  kanji_num = image_url.partition(/[0-9]+/)[1]
+
+  table = definition_agent.get(def_url).search('table')[0].to_s
+  compounds = WEBSITE + definition_agent.get(def_url).search('#articlebody').search('p').search('img')[2].attr('src')
+
+  system_call("wget -q #{image_url} && mv #{kanji_num}.gif #{image_dir}")
+  system_call("echo \"#{ROOT}/#{table}\" >> #{ROOT}/#{data_dir}/#{kanji_num}.html'")
+  system_call("wget -q #{compounds} && mv kod#{kanji_num}_2.jpg #{compound_dir}")
+
+  puts 'completed '+ kanji_num.to_s
+end
+
+
+level = ARGV[0] || 'grade2'
+data_dir     = File.join(ROOT, "data#{level}")
+image_dir    = File.join(ROOT, "image#{level}")
+compound_dir = File.join(ROOT, "compound#{level}")
+system("mkdir #{data_dir} && mkdir #{image_dir} && mkdir #{compound_dir}")
+agent = Mechanize.new
+agent.get("#{WEBSITE}blkod#{level}.htm")
+kanji = agent.page.search('#articlebody').search('a').to_a
+kanji.slice!(0, 2)
+kanji.slice!(-3, 3)
+
+for i in 80...kanji.size
+  k = kanji[i]
+  process_kanji(k, image_dir, data_dir, compound_dir)
+end
